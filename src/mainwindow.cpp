d20542564bfe34d16a8476154dc7b98bc1c7344e
Rich Wildey <wwildey@opensuseVM.localdomain>
2023-10-17 10:04:22 -0500

    Added initial QT files

---
+++ src/mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "../includes/mainwindow.h"
+#include "../gui/ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
